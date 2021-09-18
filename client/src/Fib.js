77f6b6205c896a01f1e9052f872e912cc97f4d85
shubham88fru <shubham88fru@yahoo.com>
2021-09-18 12:45:47 +0530

    More stuff

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios';
 
-class Fib extends Component {
+class Fib extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
