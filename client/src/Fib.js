1c9f28f6146bc8e901b925b1dab11d22ce7c8b7f
shubham88fru <shubham88fru@yahoo.com>
2021-09-18 12:43:27 +0530

    More stuff

---
+++ client/src/Fib.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Fib extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seenIndexes: [],
+      values: {},
+      index: '',
+    };
+  }
+
+  componentDidMount() {
+    this.fetchValues();
+    this.fetchIndexes();
+  }
+
+  async fetchValues() {
+    const values = await axios.get('/api/values/current');
+    this.setState({ values: values.data });
+  }
+
+  async fetchIndexes() {
+    const seenIndexes = await axios.get('/api/values/all');
+    this.setState({
+      seenIndexes: seenIndexes.data,
+    });
+  }
+}
---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import { React } from 'react';
+import { Link } from 'react-router-dom';
+
+const OtherPage = () => {
+  return (
+    <div>
+      I'm Some other Page!
+      <Link to="/">Go Back home</Link>
+    </div>
+  );
+};
+
+export default OtherPage;
