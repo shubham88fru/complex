ffc15a542f24c4c5558d6565e89c6a3cadab4717
shubham88fru <shubham88fru@yahoo.com>
2021-09-19 12:03:47 +0530

    wireup react code

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -9,6 +9,7 @@ class Fib extends React.Component {
       values: {},
       index: '',
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -26,6 +27,52 @@ class Fib extends React.Component {
     this.setState({
       seenIndexes: seenIndexes.data,
     });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    await axios.post('/api/values', {
+      index: this.state.index,
+    });
+    this.setState({ index: '' });
+  }
+
+  renderSeenIndexes() {
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+  }
+
+  renderValues() {
+    const entries = [];
+
+    for (let key in this.state.values) {
+      entries.push(
+        <div key={key}>
+          For Index {key} I calculated {this.state.values[key]}
+        </div>
+      );
+    }
+
+    return entries;
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter your index:</label>
+          <input
+            value={this.state.index}
+            onChange={(event) => this.setState({ index: event.target.value })}
+          />
+          <button>Submit</button>
+        </form>
+
+        <h3>Indexes I have seen</h3>
+        {this.renderSeenIndexes()}
+        <h3>Calculated values</h3>
+        {this.renderValues()}
+      </div>
+    );
   }
 }
 
